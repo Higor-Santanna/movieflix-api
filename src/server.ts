9d6ecc0bbaca0275a1a897dcb1fb26ee4c1be463
Higor Santanna Rodrigues <higorrodrigues44@outlook.com>
2024-07-13 17:09:14 -0300

    Atualização 2 13/07/2024 - Adicionado o cadastro de um filme

--- src/server.ts
+++ src/server.ts
@@ -7,16 +7,40 @@ const prisma = new PrismaClient();
 
 app.get("/movies", async (_, res) => {
     const movies = await prisma.movie.findMany({
-        orderBy:{
+        orderBy: {
             title: "asc",
         },
-        include:{
+        include: {
             genres: true,
             languages: true
         }
     });
     res.json(movies);
-})
+});
+
+app.use(express.json());
+
+app.post("/movies", async (req, res) => {
+
+    const { title, genre_id, language_id, oscar_count, release_date } = req.body;
+
+    try {
+        await prisma.movie.create({
+            data: {
+                title,
+                genre_id,
+                language_id,
+                oscar_count,
+                release_date: new Date(release_date),
+            }
+        });
+
+        res.status(201).send();
+    } catch (error) {
+        return res.status(500).send({message: "Falha ao cadastrar o filme"})
+    }
+
+});
 
 app.listen(port, () => {
     console.log(`Servidor em execução na porta ${port}`);
