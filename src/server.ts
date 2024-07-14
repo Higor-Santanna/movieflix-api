68f440e947340608fee37eda56985060a1a06331
Higor Santanna Rodrigues <higorrodrigues44@outlook.com>
2024-07-14 15:15:46 -0300

    Adicionado a função de excluir filmes

--- src/server.ts
+++ src/server.ts
@@ -55,21 +55,21 @@ app.put("/movies/:id", async (req, res) => {
     //Pegar o ID do registro que vai ser atualizado;
     const id = Number(req.params.id);
 
-    try{
+    try {
         const movie = await prisma.movie.findUnique({
             where: {
                 id
             }
         });
-    
-        if(!movie){
-            return res.status(404).send({message: "O filme não foi encontrado"})
+
+        if (!movie) {
+            return res.status(404).send({ message: "O filme não foi encontrado" })
         }
-    
-        const data = {...req.body}; // aqui ele dá um sprad ou seja pegar todos os valores que o body mandar
-    
+
+        const data = { ...req.body }; // aqui ele dá um sprad ou seja pegar todos os valores que o body mandar
+
         data.release_date = data.release_date ? new Date(data.release_date) : undefined; //Faz a verificação se o body enviou uma nova data ou não
-    
+
         //Pegar os dados do filme que será atualizado e atualizar ele no prisma
         await prisma.movie.update({
             where: {
@@ -77,13 +77,36 @@ app.put("/movies/:id", async (req, res) => {
             },
             data: data // atualiza quaisquer informações, desde que, seja informações que já estejam no banco.
         });
-    } catch(error){
+    } catch (error) {
         return res.status(500).send({ message: "Falha ao atualizar o registro" });
     }
 
     //Retornar o status correto informando que o filme foi atualizado.
     res.status(200).send({ message: "Filme atualizado com sucesso" })
 })
+
+app.delete('/movies/:id', async (req, res) => {
+    const id = Number(req.params.id);
+ 
+ 
+    try{
+      const movie = await prisma.movie.findUnique({
+      where: { id }})
+ 
+  
+ 
+      if (!movie) {
+         return res.status(404).send({ message: 'Filme não encontrado' });
+      }
+ 
+        await prisma.movie.delete({ where: { id }});
+    
+      }catch(error) {
+        res.status(500).send({ message: 'Falha ao remover o registro' });
+      }
+    
+  res.status(200).send({ message: 'Filme removido com sucesso' });
+ });
 
 app.listen(port, () => {
     console.log(`Servidor em execução na porta ${port}`);
